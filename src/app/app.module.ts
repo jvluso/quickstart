0f9f9dcdb0bdefbf171545ccd1730038cf61b516
Jeremy <jmacaluso@actiance.com>
2017-02-11 09:45:31 -0800

    at step 3 of instructons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,48 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }      from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
+
+
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  providers: [
+    HeroService
+  ],
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ])
+  ],
+  declarations: [ 
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
+  
+  
 export class AppModule { }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+
+
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'flash' },
+  { id: 12, name: 'Arrow' },
+  { id: 13, name: 'Martian' },
+  { id: 14, name: 'Firestorm' }
+];
